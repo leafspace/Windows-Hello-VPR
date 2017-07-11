412db5c38ed98949a6e16a93d40d63e02598bb65
leafspace <18852923073@163.com>
2017-07-11 22:27:55 +0800

    修改了分配内存的方式，添加了销毁内存的操作 Signed-off-by: leafspace <18852923073@163.com>

--- 程序代码/Windows Hello - VPR/Windows Hello - VPR/Main_Source.cpp
+++ 程序代码/Windows Hello - VPR/Windows Hello - VPR/Main_Source.cpp
@@ -9,20 +9,23 @@ int main()
 		exit(-1);
 	}
 
-	WavFile_Initial *wavFile = new WavFile_Initial(fp);                                                                                     //读取语音文件数据
+	//Todo 初始化语音文件类 读取语音文件数据
+	WavFile_Initial *wavFile = new WavFile_Initial(fp);                                                              //读取语音文件数据
 	fclose(fp);
 	for (unsigned long i = 0; i < wavFile->Get_voiceNumber(); ++i) {
 		wavFile->Pre_emphasis(wavFile->Get_dataVoicePoint(i), wavFile->Get_WavFileData());                           //对可用范围内的数据进行预加重
 	}
 
+	//Todo 初始化特征参数类 计算语音数据特征参数
 	double *dataSpace = NULL;
 	CharaParameter *charaParameter = new CharaParameter(wavFile->Get_frameNumber(wavFile->Get_dataNumber()));        //初始化特诊参数类
 	for (unsigned long i = 1; i <= wavFile->Get_frameNumber(wavFile->Get_dataNumber()); ++i) {                       //逐帧遍历
 		dataSpace = new double[WavFile_Initial::N];                                                                  //新建帧数据空间
 		wavFile->Frame_Data(wavFile->Get_WavFileData(), wavFile->Get_dataNumber(), i, dataSpace, WavFile_Initial::N);//分帧并加窗
-		charaParameter->Push_data(i, dataSpace);                                                                      //将分帧完成的数据保存进特征参数备用
+		charaParameter->Push_data(i, dataSpace);                                                                     //将分帧完成的数据保存进特征参数备用
 	}
 	delete wavFile;
+
 
 	return 0;
 }
--- 程序代码/Windows Hello - VPR/Windows Hello - VPR/WavData_CharaParameter.h
+++ 程序代码/Windows Hello - VPR/Windows Hello - VPR/WavData_CharaParameter.h
@@ -6,7 +6,16 @@ private:
 	unsigned long frameNumber;                                               //帧数量
 	double **frameData;                                                      //帧数据
 
+	//底层内存操作
+	double *DistributionSpace(unsigned long col);                            //分配一维数组空间
+	double **DistributionSpace(unsigned long row, unsigned long col);        //分配二维数组空间
+	void DestorySpace(double* space);                                        //销毁一维数组空间
+	void DestorySpace(double** space, unsigned long row);                    //销毁二位数组空间
+
+	//傅里叶变换操作
 	unsigned long fftNumber;                                                 //傅里叶变换后的数据的个数
+	double* FFT(double *data, unsigned long dataNumber);                     //快速离散傅立叶变换，无虚部 (严重警告，因为可能出现扩展内存的情况，所以必须返回新地址)
+	bool IFFT(double *data, unsigned long dataNumber);                       //快速离散逆傅立叶变换，无虚部
 public:
 	CharaParameter(unsigned long frameNumber) {
 		this->frameNumber = frameNumber;
@@ -15,8 +24,6 @@ public:
 	
 	bool Push_data(unsigned long index, double *frame);                      //初始化特征参数类使用，将index帧的数据存放如类内
 
-	double *DistributionSpace(unsigned long col);                            //分配一维数组空间
-	double **DistributionSpace(unsigned long row, unsigned long col);        //分配二维数组空间
-	double* FFT(double *data, unsigned long dataNumber);                     //快速离散傅立叶变换，无虚部 (严重警告，因为可能出现扩展内存的情况，所以必须返回新地址)
-	bool IFFT(double *data, unsigned long dataNumber);                       //快速离散逆傅立叶变换，无虚部
+	//特征参数求解列表
+	
 };
