5a3f834ef8e2ec200d3c3dad25eee4d6f68dd720
leafspace <18852923073@163.com>
2017-07-24 14:25:01 +0800

    初次添加GMM模块 Signed-off-by: leafspace <18852923073@163.com>

--- 程序代码/Windows Hello - VPR/Windows Hello - VPR/Main_Source.cpp
+++ 程序代码/Windows Hello - VPR/Windows Hello - VPR/Main_Source.cpp
@@ -1,4 +1,7 @@
 ﻿#include "WavData_CharaParameter.h"
+#include "Model_KMeans.h"
+#include "Model_GMM.h"
+#include <fstream>
 
 int main()
 {
@@ -29,4 +32,63 @@ int main()
 
 	charaParameter->MFCC_CharaParameter(sampleRate);                                                                 //计算MFCC特征参数
 	return 0;
+}
+
+void Gmm_Train()
+{
+		double data[] = {
+		0.0, 0.2, 0.4,
+		0.3, 0.2, 0.4,
+		0.4, 0.2, 0.4,
+		0.5, 0.2, 0.4,
+		5.0, 5.2, 8.4,
+		6.0, 5.2, 7.4,
+		4.0, 5.2, 4.4,
+		10.3, 10.4, 10.5,
+		10.1, 10.6, 10.7,
+		11.3, 10.2, 10.9
+	};
+
+	const int size = 10; //Number of samples
+	const int dim = 3;   //Dimension of feature
+	const int cluster_num = 4; //Cluster number
+
+	KMeans* kmeans = new KMeans(dim, cluster_num);
+	int* labels = new int[size];
+	kmeans->SetInitMode(KMeans::InitUniform);
+	kmeans->Cluster(data, size, labels);
+
+	printf("Clustering result by k-meams:\n");
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%f, %f, %f belongs to %d cluster\n", data[i*dim + 0], data[i*dim + 1], data[i*dim + 2], labels[i]);
+	}
+
+	double test_data[4][3];
+	for (int i = 0; i < 4; ++i) {
+		double *temp = kmeans->GetMean(i);
+		for (int j = 0; j < 3; ++j) {
+			test_data[i][j] = temp[j];
+		}
+	}
+
+	delete[]labels;
+	delete kmeans;
+
+	GMM *gmm = new GMM(dim, 3); //GMM has 3 SGM
+	gmm->Train(data, size);     //Training GMM
+
+	printf("\nTest GMM:\n");
+	for (int i = 0; i < 4; ++i)
+	{
+		printf("The Probability of %f, %f, %f  is %f \n", test_data[i][0], test_data[i][1], test_data[i][2], gmm->GetProbability(test_data[i]));
+	}
+
+	//save GMM to file
+	ofstream gmm_file("gmm.txt");
+	assert(gmm_file);
+	gmm_file << *gmm;
+	gmm_file.close();
+
+	delete gmm;
 }
