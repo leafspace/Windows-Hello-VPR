c558eeaaa2288c4b70a8d5049c09e33bc1a7d4a7
leafspace <18852923073@163.com>
2017-08-01 20:47:11 +0800

    添加了声纹识别桌面应用 Signed-off-by: leafspace <18852923073@163.com>

---
+++ 程序代码/Voiceprint Recognition/Voiceprint Recognition/Voiceprint Recognition.h
@@ -0,0 +1,32 @@
+
+// Voiceprint Recognition.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CVoiceprintRecognitionApp:
+// 有关此类的实现，请参阅 Voiceprint Recognition.cpp
+//
+
+class CVoiceprintRecognitionApp : public CWinApp
+{
+public:
+	CVoiceprintRecognitionApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CVoiceprintRecognitionApp theApp;
