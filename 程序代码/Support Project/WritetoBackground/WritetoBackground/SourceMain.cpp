ee0ec48869efa5271320c4e2a73ff7dd1c20bc0d
leafspace <18852923073@163.com>
2017-08-08 10:52:51 +0800

    添加了完成后台书写键盘事件的参考代码 Signed-off-by: leafspace <18852923073@163.com>

---
+++ 程序代码/Support Project/WritetoBackground/WritetoBackground/SourceMain.cpp
@@ -0,0 +1,30 @@
+#include <afx.h>
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	/*
+	HWND hWnd = ::FindWindowW(L"LogonUI", NULL);                                                                     //搜索记事本程序主窗口句柄 
+	HWND hWndc = ::GetWindow(hWnd, GW_CHILD);                                                                        //获得记事本客户区句柄（该窗口是记事本主窗口的子窗口，即那个白色的可编辑区域） 
+
+	Sleep(5000);
+	char password[256] = "sandaozhishu429";
+	if(hWndc) {                                                                                                      //如果获得了该句柄
+		for (int i = 0; i < strlen(password); ++i) {
+			::SendMessage(hWnd, WM_CHAR, password[i], NULL);                                                         //发送按键消息
+		}
+	}
+	*/
+	Sleep(5000);
+	HWND wnd;                                                                                                        //窗口句柄
+	wnd = GetForegroundWindow();                                                                                     //获得当前激活的窗口句柄
+	DWORD SelfThreadId = GetCurrentThreadId();                                                                       //获取本身的线程ID
+	DWORD ForeThreadId = GetWindowThreadProcessId(wnd, NULL);                                                        //根据窗口句柄获取线程ID
+	AttachThreadInput(ForeThreadId, SelfThreadId, true);                                                             //附加线程
+	wnd = GetFocus();                                                                                                //获取具有输入焦点的窗口句柄
+	AttachThreadInput(ForeThreadId, SelfThreadId, false);                                                            //取消附加的线程
+	SendMessage(wnd, WM_CHAR,WPARAM('a'), 0);                                                                        //发送一个字消息
+	return 0;
+}
